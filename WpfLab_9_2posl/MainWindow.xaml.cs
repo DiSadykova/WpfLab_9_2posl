3040fd9417dbd474e45b13e73296d19af0935d4d
user <user@DESKTOP-AOI1OV5>
2022-01-14 14:50:17 +0300

    Добавьте файлы проекта.

---
+++ WpfLab_9_2posl/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLab_9_2posl
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 1;
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex;
+            Uri uri = new Uri("White.xaml", UriKind.Relative);
+            if (styleIndex == 1)
+            {
+                uri = new Uri("Dark.xaml", UriKind.Relative);
+            }
+
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as string);
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+        private void ComboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble((sender as ComboBox).SelectedItem as string);
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSize;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontWeight == FontWeights.UltraBold)
+                {
+                    textBox.FontWeight = FontWeights.Normal;
+                }
+                else
+                {
+                    textBox.FontWeight = FontWeights.UltraBold;
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontStyle == FontStyles.Italic)
+                {
+                    textBox.FontStyle = FontStyles.Normal;
+                }
+                else
+                {
+                    textBox.FontStyle = FontStyles.Italic;
+                }
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.TextDecorations == TextDecorations.Baseline)
+                {
+                    textBox.TextDecorations = null;
+                }
+                else
+                {
+                    textBox.TextDecorations = TextDecorations.Baseline;
+                }
+            }
+        }
+
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void RadioButton_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = new SolidColorBrush(Colors.Red);
+            }
+        }
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Teкстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt| Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+    }
+}
